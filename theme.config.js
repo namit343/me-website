4a985c13e24ab98f2c51912145d40ab1718ff1e0
namit343 <namitd@namit.dev>
2022-10-25 19:21:02 +0530

    Updated index.mdx

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Namit Dasappanavar.
-      
+        This website was made using [Next.js](nextjs.org)
       <style jsx>{`
         a {
           float: right;
